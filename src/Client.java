5592817b4d51b16d70043cdc87f866c9ce653093
Dan <skafdan@gmail.com>
2021-08-30 09:51:47 +1200

    Changes socket types to SSL

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,21 @@
 import java.net.*;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 public class Client {
 
     public static void main(String[] args){
         try{
             int port = Integer.parseInt(args[0]);
             String host = args[1];
-            Socket socket = new Socket(host, port);
+            //Socket socket = new Socket(host, port);
+            SSLSocketFactory factory = 
+                (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket sslSocket = (SSLSocket) factory.createSocket(host,port);
             System.err.println("Connected to " + host + " on port" + port);
-            new ReadWriteThread(System.in, socket.getOutputStream(),"").start();
-            new ReadWriteThread(socket.getInputStream(), System.out,"--> ").start();
+            new ReadWriteThread(System.in, sslSocket.getOutputStream(),"").start();
+            new ReadWriteThread(sslSocket.getInputStream(), System.out,"--> ").start();
         } catch (Exception e){
             e.printStackTrace();
             System.err.println("Usage: java Client <port> <host>");
