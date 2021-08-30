2c945cfdac79d60ae05dad2087b2a1e7346ae8a4
Dan <skafdan@gmail.com>
2021-08-30 14:06:41 +1200

    Programmatically add keys to client.

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 public class Client {
 
@@ -7,8 +15,7 @@ public class Client {
         try{
             int port = Integer.parseInt(args[0]);
             String host = args[1];
-            SSLSocketFactory factory = 
-                (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocketFactory factory = SSLFactoryBootstrapper();
             SSLSocket sslSocket = (SSLSocket) factory.createSocket(host,port);
             System.err.println("Connected to " + host + " on port" + port);
             new ReadWriteThread(System.in, sslSocket.getOutputStream(),"").start();
@@ -16,6 +23,32 @@ public class Client {
         } catch (Exception e){
             e.printStackTrace();
             System.err.println("Usage: java Client <port> <host>");
+        }
+    }
+
+    public static SSLSocketFactory SSLFactoryBootstrapper(){
+        try{
+            final char[] password = "quack1nce4^".toCharArray();
+
+            final KeyStore keyStore =
+                 KeyStore.getInstance(new File("./yourKEYSTORE"), password);
+
+            final TrustManagerFactory trustManagerFactory = 
+                TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+
+            final KeyManagerFactory keyManagerFactory = 
+                KeyManagerFactory.getInstance("NewSunX509");
+            keyManagerFactory.init(keyStore, password);
+
+            final SSLContext context = SSLContext.getInstance("TLS");
+            context.init(keyManagerFactory.getKeyManagers(), 
+                trustManagerFactory.getTrustManagers(),null);
+            return context.getSocketFactory();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
         }
     }
 }
