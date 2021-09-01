bd988250269dc582d9513639d438355cbf56aae3
Dan <skafdan@gmail.com>
2021-09-01 17:19:24 +1200

    Copies key to http server

--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+while getopts 't' flag; do
+    case "${flag}" in
+        t) t_flag='true' ;;
+    esac
+done
 
 #Ensure ClassPATH set
 if [ -z "$CLASSPATH" ]
@@ -12,6 +17,9 @@ if [ ! -f "$FILE" ];
 then 
     keytool -genkey -noprompt -dname "CN=chatserver, OU=chatserver, O=chatserver, L=Unknown, ST=Unknown, C=Unknown" -keystore yourKEYSTORE -storepass quack1nce4^ -keypass quack1nce4^ -keyalg RSA -validity 360
 fi
+
+#Copy file to http server
+cp ./yourKEYSTORE /var/www/html/
 
 #Starting the server
 if [ -z "$1" ]
